078b355631af5f16ada62c7aa503e0879cdaeea6
Austin Jia <email.austin.jia@gmail.com>
2022-11-10 21:38:08 -0600

    added more data sets and accurate carbon

--- src/SimulationRunner.java
+++ src/SimulationRunner.java
@@ -13,14 +13,18 @@ import java.util.TreeMap;
 
 public class SimulationRunner {
     public static void main(String[] args) throws IOException {
+        double cpu_util, carbon, energy, power;
+        double exe_time;
+        cpu_util = 0.5;
+        power = Math.round((1.37*cpu_util + 41.32)*100)/100;
         //carbon output in tons e-6
         Model[] models = {new Model("GPT13B", 17, 1.5f, 3, 0.5f),
                           new Model("GENGPT2", 10, 0.5f, 3, 0.5f)};
 
-        CSVReader austinData = new CSVReader("ERCOT_AUSTIN_2022-03_MOER.txt");
+        CSVReader austinData = new CSVReader("CAISO_NORTH_2022-08_MOER.txt");
 
-        //TreeMap<Timestamp, ArrayList<Request>> simulatedMonth = generateRequests(1000, models, austinData);
-        TreeMap<Timestamp, ArrayList<Request>> simulatedMonth = readRequests("Requests.txt", austinData);
+        TreeMap<Timestamp, ArrayList<Request>> simulatedMonth = generateRequests(1000, models, austinData);
+        //TreeMap<Timestamp, ArrayList<Request>> simulatedMonth = readRequests("Requests.txt", austinData);
 
         float optimized = 0f;
         float notOptimized = 0f;
@@ -30,11 +34,18 @@ public class SimulationRunner {
             if(simulatedMonth.get(t).size() > 0) {
                 for (Request request : simulatedMonth.get(t)) {
                     if(request.getMOER() > austinData.getAverageMOER()){
-                        optimized += models[1].getEstimatedCarbonOutput();
+                        exe_time = models[1].getEstimatedRuntime();
                     }else{
-                        optimized += models[0].getEstimatedCarbonOutput();
+                        exe_time = models[0].getEstimatedRuntime();
                     }
-                    notOptimized += models[0].getEstimatedCarbonOutput();
+                    energy = power*exe_time / 1000 / 3600; //kilowatt hours
+                    carbon = energy/1000 * austinData.getMOER(t) * 453.592;
+                    optimized += carbon;
+
+                    exe_time = models[0].getEstimatedRuntime();
+                    energy = power*exe_time / 1000 / 3600; //kilowatt hours
+                    carbon = energy/1000 * austinData.getMOER(t) * 453.592;
+                    notOptimized += carbon;
                 }
             }
             System.out.println("Optimized: " + (Math.round(optimized*100)/100f));
